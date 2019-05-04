5b52044f8442f16ca5d488ed44ebdcd02edcfb7f
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-04 13:22:50 -0500

    installed dependencies fs and https

---
+++ index.js
@@ -0,0 +1,11 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", ws => {
+  ws.on("message", message => {
+    console.log(`Recieved ${message}`);
+  });
+
+  ws.send("something");
+});
