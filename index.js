1dd6a1aceaa5ffdbf6c079643213f871a77288ca
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-04 13:42:59 -0500

    installed nodemon as regular dep because I switch machines frequently

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+const express = require("express");
+const app = express();
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ app });
+
+app.get("/", (req, res) => {
+  res.send("it's online");
+});
 
 wss.on("connection", ws => {
   ws.on("message", message => {
@@ -8,4 +14,9 @@ wss.on("connection", ws => {
   });
 
   ws.send("something");
+});
+
+const port = 8080;
+app.listen(port, () => {
+  console.log(`\n***Listening on port ${port}***\n`);
 });
