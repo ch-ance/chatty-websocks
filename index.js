5fb9efcec91df5999abd8822b8292bcc438ae036
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-05-04 18:15:54 -0500

    Working on sending url query to server

--- index.js
+++ index.js
@@ -14,12 +14,20 @@ server.get("/", (req, res) => {
   res.send("it's online");
 });
 
-wss.on("connection", function connection(ws) {
+let clientID;
+
+wss.on("connection", function connection(ws, req) {
+  console.log("Connecting");
+  console.log(req.url);
   ws.on("message", function incoming(data) {
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
+    if (typeof data === Number) {
+      clientID = data;
+    } else {
+      wss.clients.forEach(function each(client) {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(data);
+        }
+      });
+    }
   });
 });
