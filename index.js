c1f35aa14588025bd1d2c11346f6b32a6a39f799
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-08 12:32:32 -0500

    deploying

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const WebSocket = require("ws");
+const express = require('express')
+const http = require('http')
+const app = express()
+const WebSocket = require('ws').Server
 
-const port = process.env.PORT || 3040;
+const port = process.env.PORT || 1234
 
-// these ports are differnet. Need to get ws working with express
-app.use(cors());
+app.use(express.static(__dirname + '/'))
 
-const server = app.listen(port, () => {
-  console.log(`\n***Listening on port ${port}***\n`);
-});
+const server = http.createServer(app)
 
-const wss = new WebSocket.Server({ server });
+server.listen(port)
 
-app.get("/", (req, res) => {
-  res.send("it's online");
-});
+// app.get('/', (req, res) => {
+// res.send("it's online")
+// })
 
-wss.on("connection", function connection(ws, req) {
-  console.log("Connecting");
-  ws.on("message", function incoming(data) {
-    // if message sent is a userIDMessage, meaning it's sent to set the user ID
-    const dataObject = JSON.parse(data);
-    if (dataObject.identifier) {
-      ws.id = dataObject.userID;
-      console.log(ws.id);
-    } else {
-      // for now, else just means that the message is a chat message and not meant to set the user's ID
-      const friendID = dataObject.friendID;
-      wss.clients.forEach(function each(friend) {
-        if (
-          friend !== ws &&
-          friend.readyState === WebSocket.OPEN &&
-          friend.id === friendID
-        ) {
-          console.log("FRIEND ID:   ", friend.id);
-          console.log("friendID: ", friendID);
-          console.log("MY ID -------", ws.id);
-          friend.send(data);
+const wss = new WebSocket({ server: server })
+
+wss.on('connection', function connection(ws, req) {
+    console.log('Connecting')
+    ws.on('message', function incoming(data) {
+        // if message sent is a userIDMessage, meaning it's sent to set the user ID
+        const dataObject = JSON.parse(data)
+        if (dataObject.identifier) {
+            ws.id = dataObject.userID
+            console.log(ws.id)
+        } else {
+            // for now, else just means that the message is a chat message and not meant to set the user's ID
+            const friendID = dataObject.friendID
+            wss.clients.forEach(function each(friend) {
+                console.log('a message is being transmitted')
+                console.log('Friend = ws? :', friend === ws)
+                // console.log('WS: ', ws)
+                console.log('READY STATE: ', friend.readyState)
+                console.log('friend.id: ', friend.id)
+                console.log('friendID: ', friendID)
+                if (
+                    friend !== ws &&
+                    friend.readyState === 1 &&
+                    friend.id === friendID
+                ) {
+                    console.log('FRIEND ID:   ', friend.id)
+                    console.log('friendID: ', friendID)
+                    console.log('MY ID -------', ws.id)
+                    friend.send(data)
+                }
+            })
         }
-      });
-    }
-  });
-});
+    })
+})
