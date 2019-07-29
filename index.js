018334fe86113431a588391238b617fee183276e
Chance Embrey-Farquhar <cembreyfarquhar@gmail.com>
2019-07-28 20:04:29 -0500

    correctly sends ping when a contact request is accepted

--- index.js
+++ index.js
@@ -23,7 +23,6 @@ wss.on('connection', function connection(ws, req) {
         // if message sent is a userIDMessage, meaning it's sent to set the user ID
         const dataObject = JSON.parse(data)
         if (dataObject.identifier) {
-            console.log('setting id', dataObject)
             ws.id = dataObject.username
             console.log(ws.id)
         }
@@ -37,37 +36,17 @@ wss.on('connection', function connection(ws, req) {
             })
         }
         // if packet is accepting a contact request
-        else if (dataObject.acceptingRequest) {
-            const contact = dataObject.contact
-            const user = dataObject.user
-            wss.clients.forEach(client => {
-                console.log('accepting request: ', contact, user)
-                if (client.id === contact && client.id !== ws.id) {
-                    console.log('HERE MOTHER FUCKER')
-                    console.log('CLIENT ID: ', client.id)
-                    console.log('USER: ', user)
-                    ws.send(
-                        JSON.stringify({
-                            contactRequestAccepted: true,
-                            user,
-                            contact,
-                        }),
-                    )
-                }
-            })
-        }
+
         // if packet is checking for online contacts
         else if (dataObject.statusCheck) {
             const contactIDs = dataObject.contactIDs
             const onlineContacts = []
             wss.clients.forEach(contact => {
-                console.log('hey contact, ', contactIDs)
                 if (
                     contactIDs.indexOf(contact.id !== -1) &&
                     contact.id !== ws.id &&
                     dataObject.online
                 ) {
-                    console.log('incoming: ', dataObject)
                     onlineContacts.push(contact.id)
                     const message = {
                         userIsOnline: true,
@@ -83,19 +62,29 @@ wss.on('connection', function connection(ws, req) {
             }
             wss.clients.forEach(contact => {
                 if (contact.id === ws.id) {
-                    console.log('Sending it: ', onlineStatusMessageToClient)
                     ws.send(JSON.stringify(onlineStatusMessageToClient))
                 }
             })
         }
 
         // type: 'Accepting Contact Request',
-        // acceptingUser: localStorage.getItem(
-        //     'username',
-        // ),
-        // requestingUser: from,
+        //  acceptingUser: localStorage.getItem(
+        //                        'username',
+        //                            ),
+        //         requestingUser: from,
         else if (dataObject.type === 'Accepting Contact Request') {
             console.table(dataObject)
+            const requestingUser = dataObject.requestingUser
+            wss.clients.forEach(function each(contact) {
+                if (
+                    contact !== ws &&
+                    contact.readyState === 1 &&
+                    contact.id === requestingUser
+                ) {
+                    console.log('Sending data to: ', contact.id)
+                    contact.send(data)
+                }
+            })
         } else if (dataObject.type === 'Private Message') {
             // for now, else just means that the message is a chat message and not meant to set the user's ID
             const receivingUser = dataObject.receivingUser
@@ -107,8 +96,6 @@ wss.on('connection', function connection(ws, req) {
                     contact.id === receivingUser
                     // friend.id === friendID
                 ) {
-                    console.log('DATA: ', data)
-                    console.log('CONTACT: ', contact.id)
                     contact.send(data)
                 }
             })
